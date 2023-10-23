37e44cdc26734e7c7997f20f1c257523698516bb
Nazukx <marcel.jaeger.23@hotmail.com>
2023-10-23 20:46:36 +0200

    Initial Commit

---
+++ Vocablo/VocabloApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabloApp.swift
+//  Vocablo
+//
+//  Created by Marcel Jäger on 23.10.23.
+//
+
+import SwiftUI
+
+@main
+struct VocabloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
