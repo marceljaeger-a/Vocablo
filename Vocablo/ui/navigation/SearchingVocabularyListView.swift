91ccc36e941cffac44fbb34bdab10d6e568af327
Nazukx <marcel.jaeger.23@hotmail.com>
2024-03-13 13:44:21 +0100

    Replace the filter method implementation with a predicate for the Query in the SearchingVocabularyListView.

--- Vocablo/ui/navigation/SearchingVocabularyListView.swift
+++ Vocablo/ui/navigation/SearchingVocabularyListView.swift
@@ -13,19 +13,7 @@ struct SearchingVocabularyListView: View {
     
     //MARK: - Dependencies
     
-    var searchingFilter: Optional<(Vocabulary, String) -> Bool> = nil
-    var searchingText: Optional<String> = nil
-    
     @Query private var vocabularies: Array<Vocabulary>
-    private var filteredVocabularies: Array<Vocabulary> {
-        if let searchingFilter, let searchingText{
-            return vocabularies.filter {
-                searchingFilter($0, searchingText)
-            }
-        }else {
-            return vocabularies
-        }
-    }
     
     @State private var selectedVocabularies: Set<Vocabulary> = []
     @State private var editedVocabulary: Vocabulary? = nil
@@ -33,26 +21,23 @@ struct SearchingVocabularyListView: View {
     //MARK: - Initialiser
     
     init(
-        query: Query<Vocabulary, Array<Vocabulary>> = .init(.vocabularies()),
         searchingText: String
     ) {
-        self._vocabularies = query
-        
-        self.searchingText = searchingText
-        self.searchingFilter = {
-            vocabulary, searchingText in
-                vocabulary.baseWord.caseInsensitiveContains(searchingText) ||
-                vocabulary.translationWord.caseInsensitiveContains(searchingText) ||
-                vocabulary.baseSentence.caseInsensitiveContains(searchingText) ||
-                vocabulary.translationSentence.caseInsensitiveContains(searchingText)
+        let predicate: Predicate<Vocabulary> = #Predicate { vocabulary in
+            vocabulary.baseWord.localizedStandardContains(searchingText) ||
+            vocabulary.translationWord.localizedStandardContains(searchingText) ||
+            vocabulary.baseSentence.localizedStandardContains(searchingText) ||
+            vocabulary.translationSentence.localizedStandardContains(searchingText)
         }
+        
+        self._vocabularies = Query(filter: predicate)
     }
     
     //MARK: - Body
     
     var body: some View {
         let _ = Self._printChanges()
-        VocabularyListView(vocabularies: filteredVocabularies, selectedVocabularies: $selectedVocabularies, onSubmitRow: {})
+        VocabularyListView(vocabularies: vocabularies, selectedVocabularies: $selectedVocabularies, onSubmitRow: {})
             .contextMenu(forSelectionType: Vocabulary.self) { vocabularies in
                 VocabularyListViewContextMenu(vocabulariesOfContextMenu: vocabularies, of: nil, selectedVocabularies: $selectedVocabularies, editedVocabulary: $editedVocabulary, isSearching: true)
             } primaryAction: { vocabularies in
